d17ae2d21c63a6fef519ff6d349f86c031d38721
hoangpham96 <hoangpham96@gmail.com>
2017-06-07 23:16:08 +1000

    register boiler plate servlet

--- src/elec3610/servlet/FetchInfoServlet.java
+++ src/elec3610/servlet/FetchInfoServlet.java
@@ -6,21 +6,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.ArrayList;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class LoginServlet extends HttpServlet {
+/***
+ * Get the information of the user without storing into cookies
+ * @author Hoang
+ *
+ */
+public class FetchInfoServlet extends HttpServlet {
 protected void doGet(HttpServletRequest request,
 					HttpServletResponse response) throws ServletException {
 	
 	String username = request.getParameter("username");
-	String password = request.getParameter("password");
 	String name = "";
-	boolean success = false;
+	String gender = "";
+	String address = "";
+	String email = "";
+	
+	ArrayList gameID = new ArrayList<Integer>();
+	
+	ArrayList paymentType = new ArrayList<String>();
 	
 	try {		
-		if (username != null && password != null){
+		if (username != null){
 			Class.forName("com.mysql.jdbc.Driver");
 			String mysqlUrl = "jdbc:mysql://localhost:3306/elec3610";
 			Properties userInfo = new Properties();
@@ -28,41 +39,30 @@ protected void doGet(HttpServletRequest request,
 			userInfo.put("password", "root");
 			Connection connection = DriverManager.getConnection(mysqlUrl,userInfo);
 			Statement stmt = connection.createStatement();
-			String sql = "SELECT * FROM user WHERE username = '"+username+"' and password = '"+password+"';";
+			String sql = "SELECT * FROM user WHERE username = '"+username+"';";
 			ResultSet result = stmt.executeQuery(sql);
 			result.first();
 			if (result.getRow() > 0){
-				success = true;
 				name = result.getString(4)+" "+result.getString(5);
+				gender = result.getString(6);
+				address = result.getString(6);
+				email = result.getString(8);
 			}
 			result.close();
 		}
-		else{
-			success = false;
-		}
+		
+		Cookie usernameCookie = new Cookie("username",username);
+	    response.addCookie(usernameCookie);
+	    Cookie nameCookie = new Cookie("name",name);
+	    response.addCookie(nameCookie);
+	    
+	    PrintWriter out = response.getWriter();  
+	    response.setContentType("text/html");  
+	    out.println("<script type=\"text/javascript\">");  
+	    out.println("alert('Log in successful!');");
+	    out.println("window.location = 'account_page.html';");  
+	    out.println("</script>");
 
-		if (success){
-			Cookie usernameCookie = new Cookie("username",username);
-		    response.addCookie(usernameCookie);
-		    Cookie nameCookie = new Cookie("name",name);
-		    response.addCookie(nameCookie);
-		    
-		    PrintWriter out = response.getWriter();  
-		    response.setContentType("text/html");  
-		    out.println("<script type=\"text/javascript\">");  
-		    out.println("alert('Log in successful!');");
-		    out.println("window.location = 'account_page.html';");  
-		    out.println("</script>");
-
-		}
-		else{
-		    PrintWriter out = response.getWriter();  
-		    response.setContentType("text/html");  
-		    out.println("<script type=\"text/javascript\">");  
-		    out.println("alert('Log in unsuccessful! Try again!');");
-		    out.println("window.location = 'main_page.html';");  
-		    out.println("</script>");
-		}
 
 	} catch (Exception e) {
 		e.printStackTrace();
--- src/elec3610/servlet/RegisterServlet.java
+++ src/elec3610/servlet/RegisterServlet.java
@@ -1,6 +1,11 @@
 package elec3610.servlet;
 
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,21 +16,45 @@ public class RegisterServlet extends HttpServlet {
 						HttpServletResponse response) throws ServletException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		String passwordcfm = request.getParameter("passwordcfm");
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
 		String gender = request.getParameter("gender");
-		String year = request.getParameter("year");
-		// response to the client
-		try {
-			PrintWriter out = response.getWriter();
-			 response.setContentType("text;html;charset=utf-8");
-			 out = response.getWriter();
-			out.print("this is the response from the server\n");
-			out.print("password:" + password+ "\n");
-			out.print("gender:" + gender+ "\n");
-			out.print("year:" + year+ "\n");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+				
+		try {		
+			if (username != null && password != null){
+				Class.forName("com.mysql.jdbc.Driver");
+				String mysqlUrl = "jdbc:mysql://localhost:3306/elec3610";
+				Properties userInfo = new Properties();
+				userInfo.put("user", "root");
+				userInfo.put("password", "root");
+				Connection connection = DriverManager.getConnection(mysqlUrl,userInfo);
+				Statement stmt = connection.createStatement();
+				String sql = "INSERT INTO user() VALUES(NULL, "+username+", "+password+", "+firstname
+						+", "+lastname+", "+gender+", "+address+", "+email+");";
+				stmt.executeUpdate(sql);
+				System.out.println("Inserted records into the table...");
+				
+			    PrintWriter out = response.getWriter();  
+			    response.setContentType("text/html");  
+			    out.println("<script type=\"text/javascript\">");
+			    out.println("alert('Register successful')"); 
+			    out.println("window.location = 'main_page.html';");  
+			    out.println("</script>");
+			}
+			else {
+				 PrintWriter out = response.getWriter();  
+				 response.setContentType("text");
+				 out.println("Test page");
+			}
+		    
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		    
+
+		
 	}
 	
 	protected void doPost(HttpServletRequest request,
