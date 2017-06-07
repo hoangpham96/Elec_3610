755920d0b81c6e55b8539a6fd5466cd63391d7c5
hoangpham96 <hoangpham96@gmail.com>
2017-06-07 21:58:28 +1000

    Add WebContent folder

---
+++ WebContent/data/cookieHandler.js
@@ -0,0 +1,22 @@
+var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+	  var value = "; " + document.cookie;
+	  var parts = value.split("; " + name + "=");
+	  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
