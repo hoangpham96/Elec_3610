9b8e7ec28f8d06fafe83635cc260add9b318fc36
hoangpham96 <hoangpham96@gmail.com>
2017-06-08 12:40:44 +1000

    Added filters to registration page.

--- WebContent/data/register.js
+++ WebContent/data/register.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){		
    $("#regSubmit").click(function(){
-	   var pass = true;
-	   
-	   //Filter:
-	   
-	   
+	   var pass = false;
+		var username = $("#rf1textbox").val();
+		var password = $("#rf2textbox").val();
+		var firstname = $("#rf3textbox").val();
+		var lastname = $("#rf4textbox").val();
+		var address = $("#rf6textbox").val();
+		var email = $("#rf7textbox").val();
+	   	   
+	   if (username.length == 0 || password.length == 0 ||firstname.length == 0 ||lastname.length == 0){
+		   alert("Please enter your required details.\nNote: Required details are marked with *");
+		   pass = false;
+	   }
+	   else if (username.length < 4 || password.length < 4){
+		   alert("Username and password must be 4 characters or more.");
+		   pass = false;
+	   }
+	   else if (!email.includes('@')){
+		   alert("Invalid email.\n Email must include @ sign");
+		   pass = false;
+	   }
 	   
 	   
 	   if (pass){
