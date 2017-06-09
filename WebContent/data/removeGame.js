af99bfcffc5c258d57cb5d6e1e43e69672d440df
hoangpham96 <hoangpham96@gmail.com>
2017-06-09 21:25:53 +1000

    continue working on removeGame

--- WebContent/data/removeGame.js
+++ WebContent/data/removeGame.js
@@ -4,11 +4,12 @@
 $(document).ready(function(){
 	//Remove game from game list
 	$(".removeButton").click(function(){
+		var user = getCookie("uid");
 		var gameToRemove = $(this).attr('id').split('-')[1];
-		var gameName = "";
+		var gameName = $(this).attr('id').split('-')[2];
 	   	var agree = confirm("This will remove "+gameName+" from your game list.\nDo you wish to continue ?");
 	   	if(agree){
-//	   		$.post("/Elec_3610/remove", {type:gameToRemove}, function(data){
+//	   		$.post("/Elec_3610/remove", {type:gameToRemove, uid:user}, function(data){
 //	   			
 //	   		});
 	   	}
@@ -18,7 +19,7 @@ $(document).ready(function(){
 	$("#removeAll").click(function(){
 	   	var agree = confirm("This will remove ALL games from your game list.\nDo you wish to continue ?");
 	   	if(agree){
-//	   		$.post("/Elec_3610/remove", {type:"all"}, function(data){
+//	   		$.post("/Elec_3610/remove", {type:"all", uid:user}, function(data){
 //	   			
 //	   		});
 	   	}
