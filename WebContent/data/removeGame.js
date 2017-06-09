da4e52f7e46da0b50ef36755fcfd5094bc04bd90
hoangpham96 <hoangpham96@gmail.com>
2017-06-09 20:03:04 +1000

    Add more code to removeGame

--- WebContent/data/removeGame.js
+++ WebContent/data/removeGame.js
@@ -2,8 +2,25 @@
  * 
  */
 $(document).ready(function(){
+	//Remove game from game list
+	$(".removeButton").click(function(){
+		var gameToRemove = $(this).attr('id').split('-')[1];
+		var gameName = "";
+	   	var agree = confirm("This will remove "+gameName+" from your game list.\nDo you wish to continue ?");
+	   	if(agree){
+//	   		$.post("/Elec_3610/remove", {type:gameToRemove}, function(data){
+//	   			
+//	   		});
+	   	}
+	})
+	
+	//Remove all games in game list.
 	$("#removeAll").click(function(){
-   	var agree = confirm("This will remove ALL games from your game list.\nDo you wish to continue ?");
-   	console.log(agree);
+	   	var agree = confirm("This will remove ALL games from your game list.\nDo you wish to continue ?");
+	   	if(agree){
+//	   		$.post("/Elec_3610/remove", {type:"all"}, function(data){
+//	   			
+//	   		});
+	   	}
 	})
 });
