73247cd5acf78bee0110e1dd5f02009cd52ae77b
hoangpham96 <hoangpham96@gmail.com>
2017-06-10 21:00:11 +1000

    Overlay visible once update is clicked

--- WebContent/data/updatePayment.js
+++ WebContent/data/updatePayment.js
@@ -2,5 +2,17 @@
  * When click on update in payment section
  */
 $(document).ready(function(){
-	$().click();
+	$("#u1390").click(function(){
+		//Show everything once update is clicked
+		$("#bgrblur").css("visibility","visible");
+		$("#paymentBox").css("visibility","visible");
+		$("#paymentInfo").css("visibility","visible");
+		$("#plusSign").css("visibility","visible");
+		$("#addNew").css("visibility","visible");
+		$(".cardType").css("visibility","visible");
+		$(".cardName").css("visibility","visible");
+		$(".cardEdit").css("visibility","visible");
+		$(".cardRemove").css("visibility","visible");
+		$("#paymentOK").css("visibility","visible");
+	});
 })	
