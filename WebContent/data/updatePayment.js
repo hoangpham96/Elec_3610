bf56233d65dbc15857ba3418074274377bf76ead
hoangpham96 <hoangpham96@gmail.com>
2017-06-11 12:58:05 +1000

    Add "add card" overlay page

--- WebContent/data/updatePayment.js
+++ WebContent/data/updatePayment.js
@@ -1,11 +1,5 @@
-/**
- * When click on update in payment section
- */
-$(document).ready(function(){
-	var payments = removeQuotes(getCookie("paymentDetail")).split("_ ");
-	payments.pop();
+function reloadPayments(payments){
 	var nPayment = payments.length;
-	
 	//Configuring the sizes
 	$("#addPayment").css({"top":130+nPayment*75});
 	$("#paymentOK").css({"top":130+nPayment*75+75});
@@ -41,13 +35,13 @@ $(document).ready(function(){
 						  "\n<div class='ax_default box_3 cardName'>\n<div class='cardNamediv'>\n</div>\n" +
 						  "<div class='text cardNameText'>\n<p><span>XXXX-XXXX-XXXX-"+lastFour+"</span></p>\n</div>\n" +
 						  "</div>\n" +
-						  "\n<div class='ax_default primary_button cardEdit'>\n" +
+						  "\n<div id='cardEdit-"+(i+1)+"' class='ax_default primary_button cardEdit'>\n" +
 						  "<div class='cardEditdiv'></div>\n" +
 						  "<div class='text cardEditText'>\n" +
 						  "<p><span>EDIT</span></p>\n" +
 						  "</div>\n" +
 						  "</div>\n" +
-						  "<div class='ax_default primary_button cardRemove'>\n" +
+						  "<div id='cardRemove-"+(i+1)+"' class='ax_default primary_button cardRemove'>\n" +
 						  "<div class='cardRemovediv'></div>\n" +
 						  "<div class='text cardRemoveText'>\n" +
 				          "<p><span>REMOVE</span></p>\n" +
@@ -57,9 +51,19 @@ $(document).ready(function(){
 			htmlCode += htmlDiv;
 		}			      
 		htmlCode += "\n</div>";
-		console.log(htmlCode);
 		$("#payments").replaceWith(htmlCode);
 	}
+}
+
+/**
+ * When click on update in payment section
+ */
+$(document).ready(function(){
+	var payments = removeQuotes(getCookie("paymentDetail")).split("_ ");
+	payments.pop();
+
+	reloadPayments(payments);
+	
 	
 	$("#u1390").click(function(){
 		//Show everything once update is clicked
@@ -70,11 +74,36 @@ $(document).ready(function(){
 		$(".payment").css("visibility","visible");
 		$("#paymentOK").css("visibility","visible");
 		
-		//Change card to real card
+		//When add card is clicked
+		$("#addPayment").click(function(){
+			$("#addPayment").css("visibility","hidden");
+			$(".payment").css("visibility","hidden");
+			$("#paymentOK").css("visibility","hidden");
+			$("#paymentBox").css("height","320px");
+			$("#addpaymentOK").css("visibility","visible");
+			$("#boxHeader").css({"top": "30px", "left": "260px"});
+			$("#boxHeaderText").replaceWith("<div id='boxHeaderText' class='text'><p><span>Add new card</span></p></div>");
+			$("#addPaymentForm").css("visibility","visible");
+			$("#addPaymentInput").css("visibility","visible");
+			
+			$("#addpaymentOK").click(function(){
+				var cardType = $("#rf1textbox").val();
+				var cardNum = $("#rf2textbox").val();
+				var cardExpiry = $("#rf3textbox").val();
+				var cardCCV = $("#rf4textbox").val();
+				console.log(cardType);
+			})
+		})
 		
 		//When edit is clicked
+		$(".cardEdit").click(function(){
+			var cardToEdit = $(this).attr('id').split('-')[1];
+		})
 		
 		//When remove is clicked
+		$(".cardRemove").click(function(){
+			var cardToRemove = $(this).attr('id').split('-')[1];
+		})
 		
 		//When OK is clicked
 		$("#paymentOK").click(function(){
